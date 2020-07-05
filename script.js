190acda9ac1b260862b66a87a573289cecf0e513
Artur <nikitsin.artur@gmail.com>
2020-07-06 00:15:15 +0300

    feature: create project structure. Add people fetch and render function"

---
+++ script.js
@@ -0,0 +1,64 @@
+window.onload = function () {
+
+    document.querySelector("#nav-root-tab").addEventListener("click", getPeople, false)
+    document.querySelector("#nav-people-tab").addEventListener("click", getPeople, false)
+    document.querySelector("#nav-films-tab").addEventListener("click", getPeople, false)
+    document.querySelector("#nav-starships-tab").addEventListener("click", getPeople, false)
+    document.querySelector("#nav-vehicles-tab").addEventListener("click", getPeople, false)
+    document.querySelector("#nav-species-tab").addEventListener("click", getPeople, false)
+    document.querySelector("#nav-planets-tab").addEventListener("click", getPeople, false)
+
+   /* let url = '';
+    let tab = null;
+
+    function getData(param) {
+
+        let currentTab = this.id;
+        console.log(this.id);
+
+        switch (currentTab) {
+            case "nav-films-tab":
+                tab = "films"
+                getPeople();
+                break
+            case "nav-people-tab":
+                tab = "people"
+                break
+        }
+
+    }*/
+
+
+    function getPeople() {
+        fetch("https://swapi.dev/api/people")
+            .then(response =>
+                response.json())
+
+            .then(people => {
+
+
+                let renderPeople = people.results.map(function (item, i) {
+
+                    return (
+                        `<div class="card bg-light mb-3" style="max-width: 18rem;">
+                        <div class="card-header">
+                         <h5 class="card-title">${item.name}</h5>
+                         </div>
+                        <div class="card-body">
+                        <h5 class="card-title"></h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                    </div>`)
+
+                });
+                return renderPeople;
+            })
+
+            .then(renderPeople => {
+
+                document.querySelector("#nav-people").innerHTML = renderPeople.join("")
+            })
+    }
+
+
+}
