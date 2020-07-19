b7d2c1c8d4362baf933ccd18f80bed51a34d8ecf
Artur <nikitsin.artur@gmail.com>
2020-07-19 19:37:25 +0300

    feature: add return to full list from search results function

--- script.js
+++ script.js
@@ -230,7 +230,7 @@ window.onload = function () {
 
     function fetchSearchRequest(req, resource) {
 
-        let searchUrl = `https://swapi.dev/api/${resource}/?search=`;
+        let searchUrl = `http://swapi.dev/api/${resource}/?search=`;
         fetch(searchUrl + req)
             .then(response => {
                 let answer = response.json();
@@ -238,13 +238,14 @@ window.onload = function () {
 
             })
             .then(answer => {
-                renderSearchResults(answer, resource)
+                setToLocalStorage((searchUrl + req + "&page=1"), answer);
+                renderFirstSearchResults(answer, resource)
             })
 
     };
 
 
-    function renderSearchResults(result, tab) {
+    function renderFirstSearchResults(result, tab) {
 
         let currentActiveTab = findActiveTab();
         let url = null;
@@ -268,6 +269,7 @@ window.onload = function () {
                 if (result.count === 0) {
                     showNoneResults("#nav-films");
                 } else {
+                    showResultsHeaders();
                     renderFilms(result);
                     addPaginator(result, tab, url);
                 }
@@ -277,6 +279,7 @@ window.onload = function () {
                 if (result.count === 0) {
                     showNoneResults("#nav-starships");
                 } else {
+                    showResultsHeaders();
                     renderStarships(result);
                     addPaginator(result, tab, url);
                 }
@@ -286,6 +289,7 @@ window.onload = function () {
                 if (result.count === 0) {
                     showNoneResults("#nav-vehicles");
                 } else {
+                    showResultsHeaders();
                     renderVehicles(result);
                     addPaginator(result, tab, url);
                 }
@@ -295,6 +299,7 @@ window.onload = function () {
                 if (result.count === 0) {
                     showNoneResults("#nav-species");
                 } else {
+                    showResultsHeaders();
                     renderSpecies(result);
                     addPaginator(result, tab, url);
                 }
@@ -304,6 +309,7 @@ window.onload = function () {
                 if (result.count === 0) {
                     showNoneResults("#nav-planets");
                 } else {
+                    showResultsHeaders();
                     renderPlanets(result);
                     addPaginator(result, tab, url);
                 }
@@ -313,11 +319,55 @@ window.onload = function () {
 
 
     function showResultsHeaders(tab) {
-        document.querySelector(".search-message").append("Search results:");
+        document.querySelector(".search-message").innerHTML =
+            `<div>
+                  <a href="#" class="return-button"> 
+                  <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrow-left-square" fill="currentColor" 
+                          xmlns="http://www.w3.org/2000/svg">
+                           <path fill-rule="evenodd" 
+                           d="M14 1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 
+                           2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+                          <path fill-rule="evenodd" 
+                          d="M8.354 11.354a.5.5 0 0 0 0-.708L5.707 8l2.647-2.646a.5.5 0 1 0-.708-.708l-3 3a.5.5 0 0 0 0 .708l3 3a.5.5 0 0 0 .708 0z"/>
+                             <path fill-rule="evenodd" d="M11.5 8a.5.5 0 0 0-.5-.5H6a.5.5 0 0 0 0 1h5a.5.5 0 0 0 .5-.5z"/>
+                  </svg>
+                  return to full list</a>
+                 <p class="search-results-header">Search results:<p/>                       
+           </div>`;
+        document.querySelector(".return-button").addEventListener("click", returnToFullList, false)
     }
 
     function deleteResultsHeaders() {
         document.querySelector(".search-message").innerHTML = "";
+    }
+
+
+    function returnToFullList(e) {
+        e.preventDefault();
+        deleteResultsHeaders();
+        let activeTab = findActiveTab();
+
+        switch (activeTab) {
+            case "nav-people":
+                getPeople();
+                break;
+            case "nav-films":
+                getFilms();
+                break;
+            case "nav-starships":
+                getStarShips();
+                break;
+            case "nav-vehicles":
+                getVehicles();
+                break;
+            case "nav-species":
+                getSpecies();
+                break;
+            case "nav-planets":
+                getPlanets();
+                break
+        }
+
     }
 
 
